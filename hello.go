d5ca8d43079f86f3c77ea3459824b35f717bc061
David Santos <davidprocode@proton.me>
2023-05-26 16:09:22 +0000

    refactor prefix to new func

--- hello.go
+++ hello.go
@@ -6,21 +6,23 @@ const prefixHelloEnglish = "Hello, "
 const prefixHelloSpanish = "Hola, "
 const prefixHelloFrench = "Bonjour, "
 
+func PrefixSelector(language string)(prefix string)  {
+	switch language {
+		case "Spanish":
+			prefix = prefixHelloSpanish
+		case "French":
+			prefix = prefixHelloFrench
+		default:
+			prefix = prefixHelloEnglish
+	}
+	return
+}
+
 func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
-
-	prefix:= prefixHelloEnglish
-
-	switch language {
-	case "Spanish":
-		prefix = prefixHelloSpanish
-	case "French":
-	prefix = prefixHelloFrench
-	}
-
-	return prefix + name
+	return PrefixSelector(language) + name
 }
 
 func main() {
